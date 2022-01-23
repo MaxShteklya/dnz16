c635f7146fa24fc8a237bdc79f4a13b72d9e7caf
Max Shteklya <maks.shtieklia@gmail.com>
2022-01-23 15:38:50 +0200

    Refactror blade layouts

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -8,6 +8,6 @@ class AppController extends Controller
 {
     public function __invoke()
     {
-        return view('main');
+        return view('app');
     }
 }
---
+++ resources/views/app.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+</head>
+<body>
+<div id="app">
+    <App></App>
+</div>
+
+<script src="{{ asset('js/app.js') }}" defer></script>
+</body>
+</html>
