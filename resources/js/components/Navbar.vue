3e6e5fec6909523628ed0d6434b75b4de6147a77
Max Shteklya <maks.shtieklia@gmail.com>
2022-01-23 15:15:24 +0200

    Installed Font Awesome, started developing layouts

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -1,6 +1,16 @@
 <template>
     <div>
         <header>
+            <div class="top-bar">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 col-md-6 text-center text-md-start">
+                            <i class="fa fa-phone"></i>ааа
+                        </div>
+                        <div class="col-12 col-md-6 text-center text-md-end"></div>
+                    </div>
+                </div>
+            </div>
             <ul class="nav">
                 <li><router-link class="link" to="/">Домашняя страница</router-link></li>
                 <li><router-link class="link" to="/page1">Страница 1</router-link></li>
